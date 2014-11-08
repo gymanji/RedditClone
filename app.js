cf728dade2e8dbf434dbc0a8d6aef79eb92f226d
Zach Reed <tzreedjr@gmail.com>
2014-11-08 14:50:14 -0500

    created service to handle posts data

---
+++ app.js
@@ -0,0 +1,26 @@
+angular.module('myNews', []).factory('posts', [function(){
+  var o = {
+    posts: []
+  };
+  return o;
+}])
+.controller('MainCtrl', ['$scope', 'posts', function($scope, posts) {
+	$scope.posts = posts.posts;
+
+	$scope.addPost = function() {
+		if(!$scope.title || $scope.title === '') { return window.alert('Title required!'); }
+		$scope.posts.push({
+    		title: $scope.title,
+    		link: $scope.link,
+    		upvotes: 0
+  		});
+  		$scope.title = '';
+  		$scope.link = '';
+	}
+
+	$scope.incrementUpvotes = function(post) {
+  		post.upvotes += 1;
+	}
+
+
+	}]);
